1b902c0df5db4137b01adcadee3e2c0435f23bba
Vladyslav Sobko <svv.ooj@gmail.com>
2026-06-23 21:39:52 +0300

    replace

--- public/sections/domains/index.ts
+++ public/sections/domains/index.ts
@@ -1,6 +1,6 @@
 import consistencyDomain from "./consistency.webp";
 import graphDomain from "./graph.png";
-import gymDomain from "./gym.webp";
+import gymDomain from "./gym.png";
 import indexDomain from "./index.png";
 import watchDomain from "./watch.webp";
 import workDomain from "./work.webp";
